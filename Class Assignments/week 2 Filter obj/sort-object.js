a03c5bef4e09840427e604acd18e1a9e3a1feccd
Davis Foderberg <dfoderberg@gmail.com>
2017-03-20 23:51:26 -0600

    Finished the sort object function

--- Class Assignments/week 2 Filter obj/sort-object.js
+++ Class Assignments/week 2 Filter obj/sort-object.js
@@ -13,7 +13,7 @@
 var getFirstTenBooks = function() {
     return JSON.parse(
         require('fs').readFileSync(__dirname + '/../books.json', 'UTF8'))
-        .slice(0, 100);
+        .slice(100, 110);
 }
 
 /** 
@@ -42,7 +42,7 @@ function sortByName(books, asc = true) {
     let mapIterator = sortingMap.values();
     for(let i = 0; i < books.length; i++){
         storeSort[mapIterator.next().value] = books[i]; // map iterator holds the location in array the book should be stored.
-        //console.log(sortingMap.get(i));
+        console.log(sortingMap.get(i));
         
     }
     if (!asc) storeSort.reverse();
@@ -50,19 +50,19 @@ function sortByName(books, asc = true) {
 }
 
 //console.log(sortByName(getFirstTenBooks(),true));
-let checkArray = sortByName(getFirstTenBooks(),false); // stores an array of sorted book objects
+let checkArray = sortByName(getFirstTenBooks(), true); // stores an array of sorted book objects
 //console.log(checkArray);
 for( let i = 0; i < checkArray.length; i++){
     //console.log("hi");
     console.log(checkArray[i]["title"] + "    is the title!!!");
+    
 }
 
 /* 
 * step 1 create a map storing the array position in key and number of strings smaller in the values
 * loop through an array comparing which value is larger. The larger string gets its value incremented
 * if strings are equal increment the value in the one with the larger array position.
-* then sort the array by the values stored matching the key that holds the array position.
-*/
+* then sort the array by the values stored matching the key that holds the array position. */
 
 
 /** 
@@ -71,8 +71,68 @@ for( let i = 0; i < checkArray.length; i++){
  * @param {Boolean} asc If true, returns list in ascending order.
  */
 function sortByAuthor(books, asc = true) {
+    storeSort = [];
+    sortingMap = new Map(); // create map called sortingMap
+    for (let i = 0; i < books.length; i++){
+        sortingMap.set(i, 0); // initalize map with all values and start all at values to 0
+        storeSort.push("error if outputed");
+    }
+    for (let i = 0; i < books.length; i++){  // iterates through every book
+        for (let j = (i+1); j < books.length; j++){  // iterates through books we want to check 
+            if (books[i]['author_data'] == undefined && books[j]['author_data'] == undefined){
+                
+                sortingMap.set(i, (sortingMap.get(i) + 1));  // increments the value of the key j by 1 when books[j] is >= to books[i]
+            }
+            else if (books[i]["author_data"] == undefined){
+                sortingMap.set(j, (sortingMap.get(j) + 1));
+            }
+            else if (books[j]["author_data"] == undefined){
+                sortingMap.set(i, (sortingMap.get(i) + 1));
+            }
+            else if (!books[i]["author_data"].length)
+            {
+                sortingMap.set(j, (sortingMap.get(j) + 1));
+            }
+            else if (!books[j]["author_data"].length)
+            {
+                sortingMap.set(i, (sortingMap.get(i) + 1));
+            }
+            else if (books[i]['author_data'][0]['name'] <= books[j]['author_data'][0]['name']){
+                console.log("here");
+                sortingMap.set(j, (sortingMap.get(j) + 1));  // increments the value of the key j by 1 when books[j] is >= to books[i]
+            }
+            else {
+                sortingMap.set(i, (sortingMap.get(i) + 1));  // increments the value of the key i by 1 when books[j] is <= to books[i]
+            }
+        }
+    }
+    let mapIterator = sortingMap.values();
+    for(let i = 0; i < books.length; i++){
+        storeSort[mapIterator.next().value] = books[i]; // map iterator holds the location in array the book should be stored.
+        console.log(sortingMap.get(i) + " is the location each object should go in array");
+        
+    }
+    if (!asc) storeSort.reverse();
+    return storeSort;
     
 }
+
+
+let check2Array = sortByAuthor(getFirstTenBooks(),true); // stores an array of sorted book objects
+//console.log(checkArray);
+for( let i = 0; i < check2Array.length; i++){
+    //console.log("hi");
+        if (check2Array[i]['author_data'] == undefined ){
+            
+            }
+            else if (!check2Array[i]["author_data"].length)
+            {
+                console.log("authordata was empty for i")
+            }
+        else console.log(check2Array[i]['author_data'][0]['name'] + "    is the author!!!");
+}
+
 /*
 * ['author_data']['name']
+if no author data what should i do. for now i am going to let it sort as it sees fit but i may want to put them in the back by iterating their key whenever i get to it.
 */
