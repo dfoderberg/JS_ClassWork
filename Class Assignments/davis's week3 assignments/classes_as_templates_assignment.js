a0275bac220dc1a96bc5c077ab283e7c62a5d3e5
Davis Foderberg <dfoderberg@gmail.com>
2017-04-01 23:21:31 -0600

    commented assignments on classes

--- Class Assignments/davis's week3 assignments/classes_as_templates_assignment.js
+++ Class Assignments/davis's week3 assignments/classes_as_templates_assignment.js
@@ -4,28 +4,28 @@
  * Place the objects into an array called 'all_objects'. 
  */
 
-class Car {
+class Car { // creates a template for a car
     constructor(color, make){
-        this.color = color;
-        this.make = make;
+        this.color = color; // the cars color is assigned what was passed in
+        this.make = make; // the cars make is assigned what was passed in
     };
 }
 
-all_objects = [];
+all_objects = []; // creates an array that will later hold 5 objects
 
-bigTruck = new Car("red", "ford");
+bigTruck = new Car("red", "ford"); // creates a new Car objects 
 littleTruck = new Car('pink', 'honda');
 sportCar = new Car('red', 'lotus');
 littleCar = new Car('grey', 'toyota');
-suv = new Car('white', 'GMC');
+suv = new Car('white', 'GMC'); // creates the last Car Object
 
-all_objects.push(bigTruck);
+all_objects.push(bigTruck); // stores each object in the array
 all_objects.push(littleCar);
 all_objects.push(littleTruck);
 all_objects.push(sportCar);
 all_objects.push(suv);
 
-console.log(all_objects.length);
+console.log(all_objects.length); // outputs the array length to the console.
 
 
 
